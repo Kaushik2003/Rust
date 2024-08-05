032f51089f04151f3b16a54bbe09359aaf9c66dd
Kaushik Samadder <70917815+Kaushik2003@users.noreply.github.com>
2024-08-05 20:54:52 +0530

    Rust part 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
     println!("Hello, world!");
+    let x: i8 =22; // -128 to 127;
+    let _y: i16 = 1233; //-32768-32767
+    let _z: i32 = 123;
+    let _a: i64 = 123;
+    let _b: i128 = 123;
+
+    let c: f64 = 123.34;
+
+    // there are i32  integer 32bits
+    // there are u32 unsigned integer 32btis
+    // there re f32 which is float 32 bits.
+
+
+    print!("Hi the values are x:{},c:{}",x,c);
+
 }
