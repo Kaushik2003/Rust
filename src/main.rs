b46728d7764fd9ad29c033fa4bc42f8d5d9e3277
Kaushik Samadder <70917815+Kaushik2003@users.noreply.github.com>
2025-05-06 01:58:44 +0530

    just a little test

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,61 @@
+
+
+//* Basic Variables and integer types */
+/* 
+
 fn main() {
     println!("Hello, world!");
-    let x: i8 =22; // -128 to 127;
-    let _y: i16 = 1233; //-32768-32767
-    let _z: i32 = 123;
+    let x: i8 =22;                          // -128 to 127;
+    let _y: i16 = 1233;     integer         //-32768-32767
+    let _z: i32 = 123;      
     let _a: i64 = 123;
     let _b: i128 = 123;
 
-    let c: f64 = 123.34;
 
-    // there are i32  integer 32bits
-    // there are u32 unsigned integer 32btis
-    // there re f32 which is float 32 bits.
+    let _as: u32=123;        unsigned
+    let c: f64 = 123.34;     floating
+
+    let is_something: bool = true;    boolean
+    //~ there are i32  integer 32bits
+    //~ there are u32 unsigned integer 32btis
+    //~ there re f32 which is float 32 bits.
 
 
-    print!("Hi the values are x:{},c:{}",x,c);
+    print!("Hi the values are \nx:{}\n,c:{}\n,qwe:{}\n",x,c,_as);
 
+}
+*/
+
+
+/* 
+fn main() {
+    let mut x: i8= 100;
+
+    for i in 0..1000{
+        x=x+100;
+    }
+
+}
+*/
+
+//*  IF Else */
+/*
+fn main() {
+ let _some: bool=false;
+ if _some{
+    println!("It is true");
+ }
+ else {
+     println!("False");
+ }
+
+
+}
+
+*/
+
+fn main() {
+
+    
+    
 }
